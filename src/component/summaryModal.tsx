d8063fc8a478d8aa6e8fb223f0ef23beb51bfcab
Fatih20 <fnri39@protonmail.com>
2022-02-05 19:07:05 +0700

    Initial layout of the summary window.

--- src/component/summaryModal.tsx
+++ src/component/summaryModal.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { useState } from "react";
 import { movies } from "../utils/types";
 import { useSelectedMovieContext } from "./context/SelectedMovieContext";
+import { IMAGE_URL } from "../config";
 
 interface IMainProps {
   show: boolean;
@@ -22,9 +23,68 @@ const Main = styled.div<IMainProps>`
 `;
 
 const SummaryContainer = styled.div`
-  background-color: yellow;
-  height: 50px;
-  width: 50px;
+  align-items: center;
+  background-color: #1a1a1a;
+  display: flex;
+  width: 80%;
+  padding: 1.25rem;
+  flex-direction: column;
+
+  @media (min-width: 900px) {
+    padding: 1.5rem;
+  }
+`;
+
+const DescriptionContainer = styled.div`
+  color: #fafafa;
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  padding: 0;
+
+  @media (min-width: 900px) {
+    padding-left: 1.25rem;
+  }
+`;
+
+const MovieTitle = styled.h2`
+  font-size: 2.25rem;
+  text-align: center;
+  @media (min-width: 900px) {
+    font-size: 3rem;
+    text-align: initial;
+  }
+`;
+
+const MovieDate = styled.p`
+  font-size: 1.25rem;
+  text-align: center;
+  @media (min-width: 900px) {
+    text-align: initial;
+  }
+`;
+
+const MovieOverview = styled.p`
+  line-height: 1.4;
+  margin: 1rem 0;
+  font-size: 1rem;
+  text-align: center;
+
+  @media (min-width: 900px) {
+    font-size: 1.25rem;
+    text-align: initial;
+  }
+`;
+
+const MoviePoster = styled.img`
+  margin-bottom: 1.25rem;
+  /* max-width: none; */
+  max-width: 300px;
+  width: 100%;
+  @media (min-width: 900px) {
+    margin-bottom: 0;
+    width: initial;
+  }
 `;
 
 export default function SummaryWindow({
@@ -34,10 +94,19 @@ export default function SummaryWindow({
   show: boolean;
   closerFunction: () => void;
 }) {
-  const { selectedMovie } = useSelectedMovieContext();
+  const {
+    selectedMovie: { title, release_date, overview, poster_path },
+  } = useSelectedMovieContext();
   return (
     <Main onClick={closerFunction} show={show}>
-      <SummaryContainer></SummaryContainer>
+      <SummaryContainer>
+        <MoviePoster src={`${IMAGE_URL}${poster_path}`} />
+        <DescriptionContainer>
+          <MovieTitle>{title}</MovieTitle>
+          <MovieDate>({release_date.slice(0, 4)})</MovieDate>
+          <MovieOverview>{overview}</MovieOverview>
+        </DescriptionContainer>
+      </SummaryContainer>
     </Main>
   );
 }
